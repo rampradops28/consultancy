2ba53997fc955a4dac74c2077d3cc16ceca58801
rampradops28 <rampradop28@gmail.com>
2025-05-23 10:38:02 +0530

    deployed

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ connectDB();
 // Middlewares
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173']
+    origin: ['consultancy-three-psi.vercel.app']
 }))
 app.use(express.json()); // parse incoming request in json format
 app.use(cookieParser())
